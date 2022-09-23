582caac1628672888003b8f308359954597010f3
LoneWolfEkb <mturkhan@yandex.ru>
2022-09-23 23:31:44 +0500

    error2

--- app/services/investing.py
+++ app/services/investing.py
@@ -4,14 +4,12 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.crud import charity_project_crud, donation_crud
-from app.models import CharityProject, Donation
+#from app.models import CharityProject, Donation
 
 
 async def investing(session: AsyncSession):
     donations = donation_crud.get_donations(session=session)
-    projects = await session.execute(
-        select(CharityProject).where(~CharityProject.fully_invested))
-    projects = projects.scalars().all()
+    projects = charity_project_crud.get_projects(session=session)
     donation_n = 0
     project_n = 0
     while donation_n < len(donations) and project_n < len(projects):
@@ -24,8 +22,8 @@ async def investing(session: AsyncSession):
         projects[project_n].invested_amount += free_for_investing
         if (donations[donation_n].full_amount ==
                 donations[donation_n].invested_amount):
-            setattr(donations[donation_n], 'fully_invested', True)
-            setattr(donations[donation_n], 'close_date', datetime.now())
+            donations[donation_n].fully_invested = True
+            donations[donation_n].close_date = datetime.now()
             donation_n += 1
         if (projects[project_n].full_amount ==
                 projects[project_n].invested_amount):
