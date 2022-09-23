7e957d06d92e4bda1b8e6b168c44c2fabf1ce255
LoneWolfEkb <mturkhan@yandex.ru>
2022-09-23 22:27:00 +0500

    7

--- app/services/investing.py
+++ app/services/investing.py
@@ -25,13 +25,13 @@ async def investing(session: AsyncSession):
         projects[project_n].invested_amount += free_for_investing
         if (donations[donation_n].full_amount ==
                 donations[donation_n].invested_amount):
-            donations[donation_n].fully_invested = True
-            donations[donation_n].close_date = datetime.now()
+            setattr(donations[donation_n], 'fully_invested', True)
+            setattr(donations[donation_n], 'close_date', datetime.now())
             donation_n += 1
         if (projects[project_n].full_amount ==
                 projects[project_n].invested_amount):
-            setattr(projects[project_n], 'fully_invested', True)
-            setattr(projects[project_n], 'close_date', datetime.now())
+            projects[project_n].fully_invested = True
+            projects[project_n].close_date = datetime.now()
             project_n += 1
     session.add_all(donations + projects)
     await session.commit()
