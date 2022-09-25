a3a81027d5672e2e49d21e6e8dbe588261e9e97b
unknown <mturkhan@yandex.ru>
2022-09-25 19:03:25 +0300

    investing

--- app/services/investing.py
+++ app/services/investing.py
@@ -15,33 +15,32 @@ async def fully_invested(obj: Union[CharityProjectDB, DonationAdminDB]) -> None:
     obj.close_date = datetime.now()
 
 
-async def investing(session: AsyncSession, donation: DonationAdminDB, project: CharityProjectDB) -> None:
+async def investing(donation: DonationAdminDB, project: CharityProjectDB) -> bool:
+    if donation.fully_invested or project.fully_invested:
+        return True
+
     donation_to_use = donation.full_amount - (donation.invested_amount or 0)
     project_required = project.full_amount - (project.invested_amount or 0)
     if project_required < donation_to_use:
-        await fully_invested(project)
         donation.invested_amount += project_required
+        await fully_invested(project)
     elif project_required > donation_to_use:
         project.invested_amount += donation_to_use
         await fully_invested(donation)
     else:
         await fully_invested(donation)
         await fully_invested(project)
-    session.add(project)
-    session.add(donation)
 
 
 async def donation_investing(session: AsyncSession, donation: DonationAdminDB) -> None:
     projects = await project_crud.get_projects_to_elaborate(session=session)
     for project in projects:
-        await investing(session=session, donation=donation, project=project)
-    await session.commit()
-    await session.refresh(donation)
+        if await investing(donation=donation, project=project):
+            break
 
 
 async def project_investing(session: AsyncSession, project: CharityProjectDB) -> None:
     donations = await donation_crud.get_donations_to_elaborate(session=session)
     for donation in donations:
-        await investing(session=session, donation=donation, project=project)
-    await session.commit()
-    await session.refresh(project)
+        if await investing(donation=donation, project=project):
+            break
