77a0f3dbbcebc447784505cfa167d42aab5b85d7
unknown <mturkhan@yandex.ru>
2022-09-25 13:51:40 +0300

    revert

--- app/crud/charity_project.py
+++ app/crud/charity_project.py
@@ -9,18 +9,18 @@ from app.models.charity_project import CharityProject
 
 class CRUDCharityProject(CRUDBase):
 
-    @staticmethod
-    async def get_project_id_by_name(
-            name: str, session: AsyncSession) -> Union[int, None]:
-        db_project_id = await session.execute(
-            select(CharityProject.id).where(CharityProject.name == name))
-        return db_project_id.scalars().first()
+	@staticmethod
+	async def get_project_id_by_name(
+			name: str, session: AsyncSession) -> Union[int, None]:
+		db_project_id = await session.execute(
+			select(CharityProject.id).where(CharityProject.name == name))
+		return db_project_id.scalars().first()
 
-    @staticmethod
-    async def get_projects_to_elaborate(session: AsyncSession):
-        projects = await session.execute(
-            select(CharityProject).where(CharityProject.fully_invested == False).order_by(CharityProject.create_date))
-        return projects.scalars().all()  # По логике, здесь нужно проверять именно на False
+	@staticmethod
+	async def get_projects_to_elaborate(session: AsyncSession):
+		projects = await session.execute(
+			select(CharityProject).where(CharityProject.fully_invested == False).order_by(CharityProject.create_date))
+		return projects.scalars().all()
 
 
 project_crud = CRUDCharityProject(CharityProject)
--- app/crud/donation.py
+++ app/crud/donation.py
@@ -8,20 +8,20 @@ from app.models import Donation, User
 
 
 class CRUDDonation(CRUDBase):
-    @staticmethod
-    async def get_by_user(
-        session: AsyncSession,
-        user: User,
-    ) -> List[Donation]:
-        reservations = await session.execute(
-            select(Donation).where(Donation.user_id == user.id))
-        return reservations.scalars().all()
+	@staticmethod
+	async def get_by_user(
+			session: AsyncSession,
+			user: User,
+	) -> List[Donation]:
+		reservations = await session.execute(
+			select(Donation).where(Donation.user_id == user.id))
+		return reservations.scalars().all()
 
-    @staticmethod
-    async def get_donations_to_elaborate(session: AsyncSession):
-        donations = await session.execute(
-            select(Donation).where(Donation.fully_invested == False).order_by(Donation.create_date))
-        return donations.scalars().all()
+	@staticmethod
+	async def get_donations_to_elaborate(session: AsyncSession):
+		donations = await session.execute(
+			select(Donation).where(Donation.fully_invested == False).order_by(Donation.create_date))
+		return donations.scalars().all()
 
 
 donation_crud = CRUDDonation(Donation)
