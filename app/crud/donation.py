705056b2cf7cc9bb64bd9bba5af5be41586cd65d
unknown <mturkhan@yandex.ru>
2022-09-25 14:08:24 +0300

    r

--- app/crud/donation.py
+++ app/crud/donation.py
@@ -20,7 +20,7 @@ class CRUDDonation(CRUDBase):
     @staticmethod
     async def get_donations_to_elaborate(session: AsyncSession):
         donations = await session.execute(
-            select(Donation).where(Donation.fully_invested == False).order_by(Donation.create_date))
+            select(Donation).where(Donation.fully_invested == False).order_by(Donation.create_date)) #noqa
         return donations.scalars().all()
 
 
