91cc7ce699170d73acdb55fae18393cf6801b7db
unknown <mturkhan@yandex.ru>
2022-09-26 16:57:54 +0300

    investing

--- app/services/investing.py
+++ app/services/investing.py
@@ -10,7 +10,7 @@ from app.schemas.donation import DonationAdminDB
 
 
 async def fully_invested(obj: Union[CharityProjectDB,
-                         DonationAdminDB]) -> None:
+                                    DonationAdminDB]) -> None:
     obj.fully_invested = True
     obj.invested_amount = obj.full_amount
     obj.close_date = datetime.now()
@@ -34,22 +34,25 @@ async def investing(donation: DonationAdminDB,
         await fully_invested(project)
 
 
-async def donation_investing(session: AsyncSession,
-                             donation: DonationAdminDB) ->\
-        List[CharityProjectDB]:
+async def donation_investing(
+    session: AsyncSession,
+    donation: DonationAdminDB) -> List[CharityProjectDB]:
     projects = await project_crud.get_projects_to_elaborate(session=session)
+    projects_elaborated = []
     for project in projects:
+        projects_elaborated.append(project)
         if await investing(donation=donation, project=project):
             break
-    return projects
+    return projects_elaborated
 
 
-async def project_investing(session: AsyncSession,
-                            project: CharityProjectDB) ->\
-        List[DonationAdminDB]:
-    donations = await donation_crud.get_donations_to_elaborate(session=  # noqa
-                                                               session)
+async def project_investing(
+    session: AsyncSession,
+    project: CharityProjectDB) -> List[DonationAdminDB]:
+    donations = await donation_crud.get_donations_to_elaborate(session=session)
+    donations_elaborated = []
     for donation in donations:
+        donations_elaborated.append(donation)
         if await investing(donation=donation, project=project):
             break
-    return donations
+    return donations_elaborated
